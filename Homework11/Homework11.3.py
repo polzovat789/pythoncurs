bcac43f3f76faa3b56789bad4913ad5582e54a05
Dzmitry Kazakou <kazakou.dzmitry88@gmail.com>
2025-03-09 19:38:34 +0300

    Homework11completed

--- Homework11/Homework11.3.py
+++ Homework11/Homework11.3.py
@@ -17,26 +17,26 @@ def typed(type):
 # Пример использования декоратора для разных типов
 
 @typed(type=str)
-def add(a, b):
+def add_str(a, b):
     return a + b
 
 
-print(add("3", 5))  # Вывод: "35"
-print(add(5, 5))  # Вывод: "55"
-print(add('a', 'b'))  # Вывод: 'ab'
+print(add_str("3", 5))  # Вывод: "35"
+print(add_str(5, 5))  # Вывод: "55"
+print(add_str('a', 'b'))  # Вывод: 'ab'
 
 
 @typed(type=int)
-def add(a, b, c):
+def add_st(a, b, c):
     return a + b + c
 
 
-print(add(5, 6, 7))  # Вывод: 18
+print(add_st(5, 6, 7))  # Вывод: 18
 
 
 @typed(type=float)
-def add(a, b, c):
+def add_strn(a, b, c):
     return a + b + c
 
 
-print(add(0.1, 0.2, 0.4))  # Вывод: 0.7000000000000001
+print(add_strn(0.1, 0.2, 0.4))  # Вывод: 0.7000000000000001
